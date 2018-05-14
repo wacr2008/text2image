e2ddd7c9969e27e7e7f08ee85a6c8d828d077eeb
wacr2008 <240145500@qq.com>
2018-05-14 14:14:05 +0800

    生成多个文件夹

--- gen_images.py
+++ gen_images.py
@@ -42,25 +42,42 @@ def genearte_images(per_class=10000, continue_gen=False, save_path='/home/dl1/da
     label_txt = f.readlines()
     f.close()
     label_index = [label.strip() for label in label_txt]
-    txt_mode = 'w' if not continue_gen else 'a'
+    txt_mode = 'rw' if not continue_gen else 'ra'
     image_counter = 1000000 if not continue_gen else 1000000 + len(os.listdir(save_path))
+
+    image_number =0
+    image_class =0
+    if not os.path.exists(save_path+str(image_class)):
+        os.mkdir(save_path+str(image_class))
+
     with open('simple.txt', txt_mode) as f:
         for color in color_list:
             count = 0
-            while count < per_class:
-                label_line = str(image_counter) + '.jpg '
-                label_line +=  str(label_index[:])
+            while count < per_class: 
+                image_number +=1
+                if(image_number>2499):
+                    image_number =0
+                    image_class +=1
+                    if not os.path.exists(save_path+str(image_class)):
+                        os.mkdir(save_path+str(image_class))
+
+                label_line = save_path+str(image_class)+"/"+str(image_counter) + ".jpg"
+                
                 text_length = 15
                 text = ' '
                 for i in range(text_length):
                     ch = characters[random.randint(0, len(characters) - 1)]
                     text += ch 
-                text += ' '                 
-
-                    
+    
+                text += ' '     
 
                 ftext = font.render(text, True, (255, 255, 255)) #, color[0])
-                f.write(label_line + '\n')
+                
+                label_line +=  ' '
+                label_line +=  text
+                label_line +=  '\n'
+                
+                f.write(label_line.encode("utf-8"))
  
                 # 载入背景图
                 bk_img = 'background/'+str(random.randint(1,5))+'.bmp' 
@@ -70,8 +87,9 @@ def genearte_images(per_class=10000, continue_gen=False, save_path='/home/dl1/da
                 screen.blit(background, (0, 0)) 
                 screen.blit(ftext, (0, 0))  
                 pygame.display.update()
-
-                pygame.image.save(screen, save_path + str(image_counter) + ".jpg")
+ 
+                        
+                pygame.image.save(screen, save_path+str(image_class)+"/" + str(image_counter) + ".jpg")
                 image_counter += 1
                 count += 1
         f.close()
@@ -95,23 +113,41 @@ def genearte_hanzi_mages(per_class=300, continue_gen=False, save_path='/home/dl1
     f.close()
     label_index = [label.strip() for label in label_txt]
     characters = label_index[1:]
-    txt_mode = 'w' if not continue_gen else 'a'
+    txt_mode = 'bw' if not continue_gen else 'ba'
+    
+    image_number=0
+    image_class=0
     image_counter = 1000000 if not continue_gen else 1000000 + len(os.listdir(save_path))
+
+    if not os.path.exists(save_path+str(image_class)):
+        os.mkdir(save_path+str(image_class))
+
     with open('hz_simple.txt', txt_mode) as f:
         for color in color_list:
             for _ in range(per_class):
                 count = 0
                 random.shuffle(characters)
                 text_length = 10
-                for i in range((len(characters))//text_length):
+                for i in range((len(characters))//text_length): 
+                    image_number +=1
+                    if(image_number>2500):
+                        image_number =0
+                        image_class +=1
+                        if not os.path.exists(save_path+str(image_class)):
+                            os.mkdir(save_path+str(image_class))
+
                     text = ' '
-                    label_line = str(image_counter) + '.jpg'
+                    label_line = save_path+str(image_class)+"/"+str(image_counter) + ".jpg"
                     for ch in characters[count:count+text_length]:
                         text += ch
-                        label_line += ' ' + str(label_index.index(ch))
+                    
                     text += ' '
+                    label_line +=  ' '
+                    label_line +=  text
+                    label_line +=  '\n'
+
                     ftext = font.render(text, True, (255,255,255)) #color[1], color[0])
-                    f.write(label_line + '\n')
+                    f.write(label_line.encode("utf-8"))
 
                     # 载入背景图
                     bk_img = 'background/'+str(random.randint(1,5))+'.bmp' 
@@ -121,13 +157,15 @@ def genearte_hanzi_mages(per_class=300, continue_gen=False, save_path='/home/dl1
                     screen.blit(background, (0, 0)) 
                     screen.blit(ftext, (0, 0))  
                     pygame.display.update()
+                     
+                     
+                    pygame.image.save(screen, save_path+str(image_class)+"/" + str(image_counter) + ".jpg")
+                    image_counter += 1 
                     
-                    pygame.image.save(screen, save_path + str(image_counter) + ".jpg")
-                    image_counter += 1
                     count += 10
         f.close()
 
 
 if __name__ == '__main__':
-    genearte_images(per_class=10000, continue_gen=False,save_path="gen_img/")
     genearte_hanzi_mages(per_class=120, continue_gen=True,save_path="gen_img_hz/") 
+    genearte_images(per_class=10000, continue_gen=False,save_path="gen_img/")
